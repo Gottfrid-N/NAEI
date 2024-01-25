9b092edb5bb567d6e83deb533846c871951fb11a
Gottfrid-N <gottfrid@familjen-nordstrom.se>
2024-01-25 10:32:50 +0100

    first working version
    All I need to do now is generalize it, so I can use it for multiple different con-alphabets.

---
+++ generate.py
@@ -0,0 +1,51 @@
+'''
+.
+,
+o
+x
+
+p = plosive
+!p = click
+!b = implosive
+p! = ejective
+n = nasal
+r = trill
+'r = tap or flap
+. = fricative
+.p = affricative
+u = approximant
+
+B = Bilabial
+p> = Labiolingual
+b> = Linguolabial
+p- = Dentolabial
+b- = Labiodental
+|- = Dental
+r = Alveolar
+k = Post-alveolar
+h = Retroflex
+b = Palatal
+G = Velar
+J = Uvular
+L = Pharyngeal
+|^ = Epiglottal
+|v = Glottal
+'''
+
+if __name__ == "__main__":
+    voices = [".", ",", "o", "x"]
+    places = ["B", "p>", "b>", "p-", "b-", "|-", "r", "k", "h", "b", "G", "J", "L", "|^", "|v"]
+    diacritics = ["p", "p!", "!p", "!b", "n", "r", "'r", ".", "u"]
+    laterals = ["", "l"]
+    glyphs = {}
+    index = 0
+    fill_char = "0"
+    prefix = "0x"
+    for voice in voices:
+        for place in places:
+            for diacritic in diacritics:
+                for lateral in laterals:
+                    glyphs[f"{voice}{place}{diacritic}{lateral}"] = f"\\U{str(hex(57344 + index)).removeprefix(prefix).rjust(8, fill_char)}"
+                    index += 1
+    print(glyphs)
+    print(len(glyphs))
